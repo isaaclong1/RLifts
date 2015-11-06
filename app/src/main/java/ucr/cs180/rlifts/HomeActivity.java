58c9a387a93e5c9d24ba744edb3ab40d6ada4337
isaaclong <isaacglong@gmail.com>
2015-11-06 15:09:53 -0800

    facebook login fix

--- app/src/main/java/ucr/cs180/rlifts/HomeActivity.java
+++ app/src/main/java/ucr/cs180/rlifts/HomeActivity.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.view.Gravity;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -83,6 +84,7 @@ public class HomeActivity extends AppCompatActivity
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
+        drawer.openDrawer(Gravity.LEFT); 
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
@@ -103,7 +105,9 @@ public class HomeActivity extends AppCompatActivity
             public void run() {
                 new Get_Driver_Message().execute();
 
-                showAlert();
+                if(flag) {
+                    showAlert();
+                }
 
                 ha.postDelayed(this, 5000);
             }
