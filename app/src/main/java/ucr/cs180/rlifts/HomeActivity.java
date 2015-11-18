e5fbcd3e2cd93f57af1ed231030fdfac2d825051
Albert Hsu <ahsu013@ucr.edu>
2015-11-17 19:44:03 -0800

    added home fragment and did some clean up in home activity

--- app/src/main/java/ucr/cs180/rlifts/HomeActivity.java
+++ app/src/main/java/ucr/cs180/rlifts/HomeActivity.java
@@ -1,18 +1,17 @@
 package ucr.cs180.rlifts;
 
+import android.app.AlertDialog;
 import android.app.Fragment;
-import android.support.v4.app.DialogFragment;
 import android.app.FragmentManager;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.Gravity;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -20,12 +19,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-
-
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -33,7 +29,9 @@ import org.json.JSONObject;
 import java.io.IOException;
 
 public class HomeActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener, ProfileFragment.OnFragmentInteractionListener, RiderFragment.OnFragmentInteractionListener, DriverFragment.OnFragmentInteractionListener {
+        implements NavigationView.OnNavigationItemSelectedListener, ProfileFragment.OnFragmentInteractionListener,
+        RiderFragment.OnFragmentInteractionListener, DriverFragment.OnFragmentInteractionListener,
+        Tutorial.OnFragmentInteractionListener {
 
     private EditText StartView;
     private EditText DestinationView;
@@ -84,7 +82,6 @@ public class HomeActivity extends AppCompatActivity
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
-        drawer.openDrawer(Gravity.LEFT);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
@@ -105,7 +102,7 @@ public class HomeActivity extends AppCompatActivity
             public void run() {
                 new Get_Driver_Message().execute();
 
-                if(flag) {
+                if (flag) {
                     showAlert();
                     flag = false;
                 }
@@ -115,6 +112,12 @@ public class HomeActivity extends AppCompatActivity
         }, 1000);
 
         new getProfileInformation().execute();
+        Fragment fragment = null;
+        fragment = Tutorial.newInstance("string1", "string2");
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+
+
     }
 
     @Override
@@ -124,6 +127,7 @@ public class HomeActivity extends AppCompatActivity
             drawer.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
+
         }
     }
 
@@ -212,13 +216,9 @@ public class HomeActivity extends AppCompatActivity
             fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
             getSupportActionBar().setTitle("Driver");
 
-        } else if (id == R.id.nav_manage) {
+        }
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        } else if(id == R.id.logout){
+         else if(id == R.id.logout){
             Intent intent = new Intent(this, LoginActivity.class);
             Toast.makeText(getApplicationContext(),
                     "Logout Successful", Toast.LENGTH_LONG).show();
@@ -245,6 +245,10 @@ public class HomeActivity extends AppCompatActivity
     public void onFragmentInteractionD(Uri uri) {
 
     }
+    @Override
+    public void onFragmentInteractionF(Uri uri) {
+    }
+
 
     private class Get_Driver_Message extends AsyncTask<String, Void, Void> {
         @Override
@@ -315,8 +319,8 @@ public class HomeActivity extends AppCompatActivity
         }
 
     }
-    private class getProfileInformation extends AsyncTask<String, Void, Void> {
 
+    private class getProfileInformation extends AsyncTask<String, Void, Void> {
         @Override
         protected Void doInBackground(String... params) {
             try {
